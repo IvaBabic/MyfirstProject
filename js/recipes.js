240f41fef5318830f4138b7b33d1f77fb17fb515
IvaBabic <ivanabakosbabic@gmail.com>
2022-10-21 15:54:45 +0200

    poslednja izmena

--- js/recipes.js
+++ js/recipes.js
@@ -1,17 +1,20 @@
 
-var elements = document.getElementsByTagName('h2');
+  var elements = document.getElementsByTagName('h2');
+  var array = [];
 
 function getInputValue() {  
     let value = document.getElementById("searchTxt").value;
    
     for (let i = 0 ; i < elements.length; i++) {
        if(elements[i].innerHTML.includes(value.toUpperCase())){
-       alert("The searched word can be found in " + elements[i].innerHTML);
-        }    
-} 
+         array.push(elements[i].innerHTML);
+       }
+      }
+      
+      if(array.length == 0) {
+        alert('We are sorry but there are no matches for your search.');
+       } 
+       else{
+        alert("The searched word can be found in " + array);
+       }
   }
-
-
-  
-  
-
